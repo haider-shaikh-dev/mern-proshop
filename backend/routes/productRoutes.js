9715c261c5f0eb95413626a178f5a2b6f06eedca
Haider Ali Mohammed Ali Shaikh <Haider.Shaikh@coforge.com>
2025-10-13 19:58:52 +0530

    product review added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,14 +7,14 @@ import {
   createProduct,
   updateProducts,
   deleteProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 
 //-> prefix is /api/products
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
-
 router.route("/:id").get(getProductById).put(updateProducts).delete(protect, admin, deleteProduct);
-
+router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
